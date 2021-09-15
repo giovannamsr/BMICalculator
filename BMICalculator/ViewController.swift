db5d7c1ec142527acbbaaf7f8b2f4d1bb012dc56
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-09-15 18:49:04 -0300

    Measure labels now change according to sliders.

--- BMICalculator/ViewController.swift
+++ BMICalculator/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    @IBAction func heightChanged(_ sender: UISlider) {
+        heightValueLabel.text = "\(String(format: "%.1f", sender.value))m"
+    }
+    @IBAction func weightChanged(_ sender: UISlider) {
+        weightValueLabel.text = "\(Int(sender.value))kg"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightSlider.value = 1.5
+        heightValueLabel.text = "1.5m"
+        weightSlider.value = 100
+        weightValueLabel.text = "100m"
+        
     }
 
-
 }
+
 
